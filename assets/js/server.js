221d24043c270a7a04dc471a46240904ef791c60
Ushkurova <nadzeja92@yandex.ru>
2021-04-04 12:45:46 +0300

    Init

---
+++ assets/js/server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function(request, response){
+    console.log(request.method, request.url);
+    var text = fs.readFileSync('..\\..\\index2.html');
+    switch(request.url)
+        {
+            case '/assets/images/Marina.PNG': 
+                text = fs.readFileSync('..\\images/Marina.PNG');
+                break;
+            case '/assets/css/main2.css': 
+                text = fs.readFileSync('..\\css/main2.css');
+                break;
+           case '/assets/css/reset.css': 
+                text = fs.readFileSync('..\\css/reset.css');
+                break;
+            case '/assets/js/main.js': 
+                text = fs.readFileSync('..\\js/main.js');
+                break;
+                
+        }
+      response.end(text);
+});
+server.listen(3000);
+console.log('Server started!');
